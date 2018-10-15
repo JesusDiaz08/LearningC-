d0dd8df9b1a28e8ce6a44d8202d98c2bc42f5d59
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-10-15 17:22:13 -0500

    Excercise 05: Excepciones

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Excepciones{
+    class Program{
+        static void Main(string[] args){
+
+            double precio, total;
+            int cantidad;
+
+            /* ... Try-Catch ... */
+            try{
+                Console.WriteLine("Cantidad: ");
+                cantidad = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Cantidad: ");
+                precio = Convert.ToDouble(Console.ReadLine());
+
+                total = cantidad * precio;
+                Console.WriteLine("Total: " + total);
+
+            }catch (FormatException error){
+                Console.WriteLine("There was an exception.\n" + error);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
