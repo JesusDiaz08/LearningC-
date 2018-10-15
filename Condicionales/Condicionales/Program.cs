2343a382dcd6f6828cef6fcefa3550b8004901fa
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-10-15 17:21:19 -0500

    Excercise 04: Condicionales

---
+++ Condicionales/Condicionales/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Condicionales{
+    class Program{
+        static void Main(string[] args){
+            byte juan = 19, pedro = 18, jose = 22;
+            if (juan > pedro) {
+                Console.WriteLine("Pedro es menor que Juan");
+                if (juan < jose) {
+                    Console.WriteLine("Pero Juan es menor que Jose");
+                }else
+                    Console.WriteLine("Y Juan es mayor que Jose");
+            
+            }else if(jose>juan && jose>pedro){
+                Console.WriteLine("Jose es el mayor de todos");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
